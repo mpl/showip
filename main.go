660abfcf8ef4fb1fe15b23d05746e8e753a92707
mpl <mathieu.lonjaret@gmail.com>
2017-07-31 01:48:27 +0200

    remove useless genselftls

--- main.go
+++ main.go
@@ -3,21 +3,13 @@
 package main
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/pem"
 	"flag"
 	"fmt"
-	"io"
 	"log"
-	"math/big"
 	"net"
 	"net/http"
 	"os"
 	"sync"
-	"time"
 
 	"github.com/mpl/basicauth"
 	"github.com/mpl/simpletls"
@@ -88,38 +80,6 @@ func recordRemoteAddrHandler(rw http.ResponseWriter, req *http.Request, url stri
 	defer mu.Unlock()
 	joIP = req.RemoteAddr
 	fmt.Fprintf(rw, joIP)
-}
-
-func genSelfTLS(certOut, keyOut io.Writer) error {
-	priv, err := rsa.GenerateKey(rand.Reader, 1024)
-	if err != nil {
-		return fmt.Errorf("failed to generate private key: %s", err)
-	}
-
-	now := time.Now()
-
-	template := x509.Certificate{
-		SerialNumber: new(big.Int).SetInt64(0),
-		Subject: pkix.Name{
-			CommonName:   *host,
-			Organization: []string{*host},
-		},
-		NotBefore: now.Add(-5 * time.Minute).UTC(),
-		NotAfter:  now.AddDate(1, 0, 0).UTC(),
-
-		SubjectKeyId: []byte{1, 2, 3, 4},
-		KeyUsage:     x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-	}
-
-	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
-	if err != nil {
-		return fmt.Errorf("Failed to create certificate: %s", err)
-	}
-
-	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
-	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
-	log.Println("self-signed cert and key generated")
-	return nil
 }
 
 func initUserPass() {
